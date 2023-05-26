29149261c9b5799c308df0330f8b46ad38514223
Amir Azmi <amir.azmi@prodigious.com>
2023-05-26 14:36:05 +0800

    Setting up basic frontend

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const db = require("./utils/database");
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
